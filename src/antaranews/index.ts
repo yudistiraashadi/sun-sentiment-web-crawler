ef8073d1f933e2bbb69650b235d740ca2cbaecfc
Yudistira Ashadi <distra96@yahoo.com>
2024-08-06 23:07:09 +0700

    chore: Update .gitignore and package.json
    - Add 'output' to .gitignore
    - Remove 'supabase' directory from .gitignore
    - Update devDependencies in package.json
    - Remove unnecessary code and comments in src/antaranews/index.ts
    - Add helper functions for date conversion in src/antaranews/index.ts
    - Update configuration in src/antaranews/index.ts
    - Add comments to explain the purpose of each section in src/antaranews/index.ts
    - Add code to save data to CSV file in src/antaranews/index.ts
    - Move CSV file to 'output' folder in src/antaranews/index.ts

--- src/antaranews/index.ts
+++ src/antaranews/index.ts
@@ -1,20 +1,73 @@
-const NEWS_SITE = "antaranews";
-
 // For more information, see https://crawlee.dev/
-import { RequestQueue, CheerioCrawler, Configuration } from "crawlee";
+import { RequestQueue, CheerioCrawler, Configuration, log } from "crawlee";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the current file
+const __dirname = path.dirname(__filename); // get the name of the current directory
+
+const PLATFORM = "antaranews";
+const MAX_BERITA_ID = 70;
 
 // CONFIGURATION
 const config = Configuration.getGlobalConfig();
 
-config.set("defaultDatasetId", NEWS_SITE);
-config.set("defaultKeyValueStoreId", NEWS_SITE);
-config.set("defaultRequestQueueId", NEWS_SITE);
-// config.set("purgeOnStart", false);
+// config.set("defaultDatasetId", PLATFORM);
+// config.set("defaultKeyValueStoreId", PLATFORM);
+// config.set("defaultRequestQueueId", PLATFORM);
+config.set("purgeOnStart", true);
+// END OF CONFIGURATION
+
+// HELPER FUNCTIONS
+function convertToDateString(dateString: string) {
+  // Define a mapping for Indonesian month names to numbers
+  const monthMap = new Map([
+    ["januari", "01"],
+    ["februari", "02"],
+    ["maret", "03"],
+    ["april", "04"],
+    ["mei", "05"],
+    ["juni", "06"],
+    ["juli", "07"],
+    ["agustus", "08"],
+    ["september", "09"],
+    ["oktober", "10"],
+    ["november", "11"],
+    ["desember", "12"],
+  ]);
+
+  // Split the string into components
+  const [_, day, month, year] = dateString
+    .trim()
+    .toLowerCase()
+    .split(/[,\s]+/);
+
+  // Get the month number
+  const monthNumber = monthMap.get(month);
+
+  if (!monthNumber) {
+    return "";
+  }
+
+  // Pad the day with a leading zero if necessary
+  const paddedDay = day.padStart(2, "0");
+
+  // Return the date in YYYY-MM-DD format
+  return `${year}-${monthNumber}-${paddedDay}`;
+}
+
+function getYyyymmddhhmmss(date: Date) {
+  const pad = (num: number) => num.toString().padStart(2, "0");
+  return `${date.getFullYear()}${pad(date.getMonth() + 1)}${pad(
+    date.getDate()
+  )}${pad(date.getHours())}${pad(date.getMinutes())}${pad(date.getSeconds())}`;
+}
+// END OF HELPER FUNCTIONS
 
 // REQUEST QUEUE
 const requestQueue = await RequestQueue.open();
 
-// initial request
 await requestQueue.addRequests([
   { url: "https://www.antaranews.com/berita/1", uniqueKey: "1" },
   { url: "https://www.antaranews.com/berita/2", uniqueKey: "2" },
@@ -27,27 +80,62 @@ await requestQueue.addRequests([
   { url: "https://www.antaranews.com/berita/9", uniqueKey: "9" },
   { url: "https://www.antaranews.com/berita/10", uniqueKey: "10" },
 ]);
+// END OF REQUEST QUEUE
 
+// CRAWLER
 const crawler = new CheerioCrawler(
   {
     // Comment this option to scrape the full website.
-    maxRequestsPerCrawl: 5,
     requestQueue: requestQueue,
     async requestHandler({ $, request, pushData }) {
       // Extract data from the page
-      const title = $("h1").text();
+      const title = $("h1").text().toLowerCase();
       const article = $(".wrap__article-detail-content")
         .contents()
         .filter(function () {
           return this.nodeType === 3;
         })
-        .text();
+        .text()
+        .trim()
+        .replace(/(\r\n|\n|\r|\t)/gm, "")
+        .toLowerCase();
+
+      const articleMetadata = $(".text-muted.mt-2.small").text().toLowerCase();
+      let author = "";
+
+      if (articleMetadata.includes("editor")) {
+        author = articleMetadata.split("editor: ")[1].split("\t")[0];
+      }
+
+      const date = convertToDateString(
+        $(".text-secondary.font-weight-normal")
+          .contents()
+          .filter(function () {
+            return this.nodeType === 3;
+          })
+          .text()
+          .trim()
+      );
 
       const beritaId = parseInt(request.uniqueKey);
       const nextBeritaId = beritaId + 10;
 
+      // if max berita id reached, stop
+      if (beritaId > MAX_BERITA_ID) {
+        return;
+      }
+
       if (title && article) {
-        pushData({ beritaId, title, article });
+        // save data
+        await pushData({
+          crawl_datetime: new Date().toISOString(),
+          platform: PLATFORM,
+          url: request.url,
+          title,
+          article,
+          author,
+          date,
+        });
       }
 
       await requestQueue.addRequests([
@@ -57,8 +145,57 @@ const crawler = new CheerioCrawler(
         },
       ]);
     },
-  }
-  // config
+  },
+  config
 );
 
 await crawler.run();
+// END OF CRAWLER
+
+// WRITE TO CSV
+await crawler.getDataset().then((dataset) => {
+  return dataset.exportToCSV(PLATFORM, { toKVS: PLATFORM });
+});
+
+// move the csv file to the output folder
+const oldPath = path.join(
+  __dirname,
+  "..",
+  "..",
+  "storage",
+  "key_value_stores",
+  PLATFORM,
+  `${PLATFORM}.csv`
+);
+const currentYyyymmddhhmmss = getYyyymmddhhmmss(new Date());
+const newPath = path.join(
+  __dirname,
+  "..",
+  "..",
+  "output",
+  `${currentYyyymmddhhmmss}_${PLATFORM}.csv`
+);
+
+// create the output folder if it doesn't exist
+fs.mkdir(
+  path.dirname(newPath),
+  {
+    recursive: true,
+  },
+  (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  }
+);
+
+fs.rename(oldPath, newPath, (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  log.info(`File moved to ${newPath}`);
+});
+// END OF WRITE TO CSV
